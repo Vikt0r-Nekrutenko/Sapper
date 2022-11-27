94daf8bde5ba5df305927845fe855b41afdf2246
Vikt0r-Nekrutenko <vikt0r.nekrut@outlook.com>
2022-11-27 11:24:03 +0200

    058

--- gamefield.hpp
+++ gamefield.hpp
@@ -9,13 +9,29 @@ class Chunk : public stf::sdb::IChunk
 public:
     static constexpr int Width  = 9;
     static constexpr int Height = 9;
+    std::vector<stf::Vec2d> mBombsPositions;
 
     Chunk() : stf::sdb::IChunk({Width, Height})
     {
         mArray.resize(Width * Height);
-        for(auto &cell : mArray) {
-            cell = new Cell;
-        }
+//        for(auto &cell : mArray) {
+//            cell = new Cell;
+//        }
+
+        int bombs = 5;
+        do {
+            for(int i = 0; i < Width * Height; ++i) {
+                const stf::Vec2d pos { i % Width, i / Width };
+                if(rand() % 100 < 10 && bombs > 0) {
+                    delete at(pos);
+                    put(pos, new BombCell);
+                    mBombsPositions.push_back(pos);
+                    --bombs;
+                } else {
+                    put(pos, new Cell);
+                }
+            }
+        } while(bombs > 0);
     }
 
     stf::sdb::IChunk *getNew() override
@@ -49,21 +65,7 @@ public:
             return curcell;
         };
 
-        int bombs = 5;
-        std::vector<stf::Vec2d> bombsPos;
-        do {
-            for(int i = 0; i < Width * Height; ++i) {
-                const stf::Vec2d pos { i % Width, i / Width };
-                if(rand() % 100 < 10 && bombs > 0 && cursor != pos) {
-                    delete at(pos);
-                    put(pos, new BombCell);
-                    bombsPos.push_back(pos);
-                    --bombs;
-                }
-            }
-        } while(bombs > 0);
-
-        for(auto &pos : bombsPos) {
+        for(auto &pos : mBombsPositions) {
             checkAroundForBombs(pos);
         }
         activate(cursor);
