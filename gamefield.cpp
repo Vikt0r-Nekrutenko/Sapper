5ec7473492a713898999d1fa75a5347e920e92f5
Vikt0r-Nekrutenko <vikt0r.nekrut@outlook.com>
2022-11-28 17:27:37 +0200

    094

--- gamefield.cpp
+++ gamefield.cpp
@@ -31,8 +31,8 @@ void GameField::onClick(const stf::Vec2d &cursor)
                 if(x<0 || y<0 || x > Width * Chunk::Width - 1 || y > Height * Chunk::Height - 1)
                     continue;
 
-                if(cell->uniqueIntView() == Cell().uniqueIntView()) {
-                    bool isBombsAroundExist = false;
+                int aroundBombCount = 0;
+//                if(cell->uniqueIntView() == Cell().uniqueIntView()) {
 
                     for(int by = pos.y - 1; by <= pos.y + 1; ++by) {
                         for(int bx = pos.x - 1; bx <= pos.x + 1; ++bx) {
@@ -41,34 +41,33 @@ void GameField::onClick(const stf::Vec2d &cursor)
                                 continue;
 
                             if(cell->uniqueIntView() == BombCell().uniqueIntView())
-                                isBombsAroundExist = true;
+                                ++aroundBombCount;
                         }
                     }
 
-                    if(!isBombsAroundExist) {
+                    if(!aroundBombCount && cell->uniqueIntView() == Cell().uniqueIntView()) {
                         put({x,y}, new EmptyCell);
                         emptyCells.push_back({x,y});
                         static_cast<Cell*>(mField.at({x,y}))->activate();
                     }
-                }
-                else if(cell->uniqueIntView() == BombCell().uniqueIntView()) {
+//                }
+                else if(aroundBombCount && cell->uniqueIntView() == BombCell().uniqueIntView()) {
                     put(pos, new BombsNeighborCell());
                     cell = static_cast<Cell*>(mField.at(pos));
                     cell->activate();
 
-                    int bombCount = 0;
-                    for(int by = pos.y - 1; by <= pos.y + 1; ++by) {
-                        for(int bx = pos.x - 1; bx <= pos.x + 1; ++bx) {
-                            Cell *neighbor = static_cast<Cell*>(mField.at({bx,by}));
+//                    for(int by = pos.y - 1; by <= pos.y + 1; ++by) {
+//                        for(int bx = pos.x - 1; bx <= pos.x + 1; ++bx) {
+//                            Cell *neighbor = static_cast<Cell*>(mField.at({bx,by}));
 
-                            if(bx<0 || by<0 || bx > Width * Chunk::Width - 1 || by > Height * Chunk::Height - 1)
-                                continue;
+//                            if(bx<0 || by<0 || bx > Width * Chunk::Width - 1 || by > Height * Chunk::Height - 1)
+//                                continue;
 
-                            if(neighbor->uniqueIntView() == BombCell().uniqueIntView())
-                                ++bombCount;
-                        }
-                    }
-                    cell->bombsAround(cell->bombsAround() + bombCount);
+//                            if(neighbor->uniqueIntView() == BombCell().uniqueIntView())
+//                                ++bombCount;
+//                        }
+//                    }
+                    cell->bombsAround(cell->bombsAround() + aroundBombCount);
                 }
             }
         }
