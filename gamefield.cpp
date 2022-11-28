fe0727cfcbbb28fcc06905d3473ff9d53933ade0
Vikt0r-Nekrutenko <vikt0r.nekrut@outlook.com>
2022-11-28 19:46:12 +0200

    097

--- gamefield.cpp
+++ gamefield.cpp
@@ -53,3 +53,79 @@ Cell *GameField::onClick(const stf::Vec2d &cursor)
     }
     return static_cast<Cell*>(mField.at(emptyCells.back()));
 }
+
+Chunk::Chunk() : stf::sdb::IChunk({Width, Height})
+{
+    mArray.resize(Width * Height, nullptr);
+    int bombs = BombsPerChunk;
+    while(bombs) {
+        stf::Vec2d pos { rand() % Width, rand() % Height };
+        if(at(pos) == nullptr) {
+            put(pos, new BombCell);
+            mBombsPositions.push_back(pos);
+            --bombs;
+        }
+    }
+    for(auto &cell : mArray) {
+        if(cell == nullptr) {
+            cell = new Cell;
+        }
+    }
+}
+
+stf::sdb::IChunk *Chunk::getNew()
+{
+    return new Chunk;
+}
+
+bool Chunk::isInitialised() const
+{
+    return mIsInitialised;
+}
+
+void Chunk::initialise()
+{
+    mIsInitialised = true;
+}
+
+stf::sdb::IChunk &Chunk::load(FILE *file)
+{
+    stf::sdb::IChunk::load(file);
+    fread(&mIsInitialised, sizeof(mIsInitialised), 1, file);
+
+    for(size_t i = 0; i < mArray.size(); ++i) {
+        switch (static_cast<Cell*>(mArray[i])->uniqueIntView()) {
+        case 1: {
+            auto tmp = new EmptyCell(*static_cast<EmptyCell *>(mArray[i]));
+            delete mArray[i];
+            mArray[i] = tmp;
+            break;
+        }
+        case 2: {
+            auto tmp = new BombCell(*static_cast<BombCell *>(mArray[i]));
+            delete mArray[i];
+            mArray[i] = tmp;
+            break;
+        }
+        case 3: {
+            auto tmp = new BombsNeighborCell(*static_cast<BombsNeighborCell *>(mArray[i]));
+            delete mArray[i];
+            mArray[i] = tmp;
+            break;
+        }
+        }
+    }
+    return *this;
+}
+
+size_t Chunk::sizeOfSelf() const
+{
+    return stf::sdb::IChunk::sizeOfSelf() + sizeof(mIsInitialised);
+}
+
+stf::sdb::IChunk &Chunk::save(FILE *file)
+{
+    stf::sdb::IChunk::save(file);
+    fwrite(&mIsInitialised, sizeof(mIsInitialised), 1, file);
+    return *this;
+}
--- gamefield.hpp
+++ gamefield.hpp
@@ -12,81 +12,15 @@ public:
     static constexpr int BombsPerChunk = 10;
     std::vector<stf::Vec2d> mBombsPositions;
 
-    Chunk() : stf::sdb::IChunk({Width, Height})
-    {
-        mArray.resize(Width * Height, nullptr);
-        int bombs = BombsPerChunk;
-        while(bombs) {
-            stf::Vec2d pos { rand() % Width, rand() % Height };
-            if(at(pos) == nullptr) {
-                put(pos, new BombCell);
-                mBombsPositions.push_back(pos);
-                --bombs;
-            }
-        }
-        for(auto &cell : mArray) {
-            if(cell == nullptr) {
-                cell = new Cell;
-            }
-        }
-    }
+    Chunk();
 
-    stf::sdb::IChunk *getNew() override
-    {
-        return new Chunk;
-    }
+    stf::sdb::IChunk *getNew() override;
+    stf::sdb::IChunk &load(FILE *file) override;
+    stf::sdb::IChunk &save(FILE *file) override;
 
-    bool isInitialised() const
-    {
-        return mIsInitialised;
-    }
-
-    void initialise()
-    {
-        mIsInitialised = true;
-    }
-
-    stf::sdb::IChunk &load(FILE *file) override
-    {
-        stf::sdb::IChunk::load(file);
-        fread(&mIsInitialised, sizeof(mIsInitialised), 1, file);
-
-        for(size_t i = 0; i < mArray.size(); ++i) {
-            switch (static_cast<Cell*>(mArray[i])->uniqueIntView()) {
-            case 1: {
-                auto tmp = new EmptyCell(*static_cast<EmptyCell *>(mArray[i]));
-                delete mArray[i];
-                mArray[i] = tmp;
-                break;
-            }
-            case 2: {
-                auto tmp = new BombCell(*static_cast<BombCell *>(mArray[i]));
-                delete mArray[i];
-                mArray[i] = tmp;
-                break;
-            }
-            case 3: {
-                auto tmp = new BombsNeighborCell(*static_cast<BombsNeighborCell *>(mArray[i]));
-                delete mArray[i];
-                mArray[i] = tmp;
-                break;
-            }
-            }
-        }
-        return *this;
-    }
-
-    size_t sizeOfSelf() const override
-    {
-        return stf::sdb::IChunk::sizeOfSelf() + sizeof(mIsInitialised);
-    }
-
-    stf::sdb::IChunk &save(FILE *file) override
-    {
-        stf::sdb::IChunk::save(file);
-        fwrite(&mIsInitialised, sizeof(mIsInitialised), 1, file);
-        return *this;
-    }
+    bool isInitialised() const;
+    void initialise();
+    size_t sizeOfSelf() const override;
 
 protected:
 
