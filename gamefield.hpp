916c8aac4bdf40593dfe7b00af4580960b5c024c
Vikt0r-Nekrutenko <vikt0r.nekrut@outlook.com>
2022-11-28 06:05:23 +0200

    078

--- gamefield.hpp
+++ gamefield.hpp
@@ -62,112 +62,16 @@ public:
     stf::sdb::ChunkedMap mField = stf::sdb::ChunkedMap({Width,Height}, &mBegin, true, "sapper.schnks");
     std::vector<stf::Vec2d> mBombsPositions;
 
+    void update();
+    void activate(const stf::Vec2d cursor);
 
-    Cell* put(const stf::Vec2d &pos, Cell* cell)
-    {
-        delete mField.at(pos);
-        return static_cast<Cell*>(mField[pos]->put(pos, cell));
-    }
+private:
 
-    void update()
-    {
-        for(auto &rec : mField.cache().chunksTable()) {
-            if(!static_cast<Chunk*>(rec.mChunkRec.mChunk)->isInitialised())
-                initChunks();
-        }
-    }
-
-    void activateCells(const stf::Vec2d &pos, std::list<stf::Vec2d> &emptyCells)
-    {
-        for(int y = pos.y-1; y <= pos.y+1; ++y) {
-            for(int x = pos.x-1; x <= pos.x+1; ++x) {
-                Cell *cell = static_cast<Cell*>(mField.at({x,y}));
-
-                if(x<0 || y<0 || x > Width * Chunk::Width - 1 || y > Height * Chunk::Height - 1)
-                    continue;
-
-                else if(cell->uniqueIntView() == Cell().uniqueIntView()) {
-                    put({x,y}, new EmptyCell);
-                    emptyCells.push_back({x,y});
-                    static_cast<Cell*>(mField.at({x,y}))->activate();
-                }
-
-                else if(cell->uniqueIntView() == BombsNeighborCell().uniqueIntView()) {
-                    cell->activate();
-                }
-            }
-        }
-    }
-
-    void activate(const stf::Vec2d cursor)
-    {
-        std::list<stf::Vec2d> emptyCells;
-
-        Cell *cell = static_cast<Cell*>(mField.at(cursor));
-        if(cell->uniqueIntView() == BombsNeighborCell().uniqueIntView()) {
-            cell->activate();
-            return;
-        }
-
-        emptyCells.push_back(cursor);
-
-        for(auto pos : emptyCells) {
-            activateCells(pos, emptyCells);
-        }
-    }
-
-    int calculateBombsAround(const int x, const int y)
-    {
-        int mineCount = 0;
-        for(int ny = y-1; ny <= y+1; ++ny) {
-            for(int nx = x-1; nx <= x+1; ++nx) {
-                Cell *neighbor = static_cast<Cell*>(mField.at({nx,ny}));
-                if(nx<0 || ny<0 || nx > Width * Chunk::Width - 1 || ny > Height * Chunk::Height - 1)
-                    continue;
-                if(nx == x && ny == y)
-                    continue;
-                if(neighbor->uniqueIntView() == BombCell().uniqueIntView())
-                    ++mineCount;
-            }
-        }
-        return mineCount;
-    }
-
-    void putBombMarkers(const stf::Vec2d &pos)
-    {
-        for(int y = pos.y-1; y <= pos.y+1; ++y) {
-            for(int x = pos.x-1; x <= pos.x+1; ++x) {
-
-                Cell *cell = static_cast<Cell*>(mField.at({x,y}));
-
-                if(x<0 || y<0 || x > Width * Chunk::Width - 1 || y > Height * Chunk::Height - 1)
-                    continue;
-
-                else if(x == pos.x && y == pos.y)
-                    continue;
-
-                else if(cell->uniqueIntView() != BombCell().uniqueIntView()) {
-                    put({x,y}, new BombsNeighborCell());
-                    cell = static_cast<Cell*>(mField.at({x,y}));
-
-                    cell->bombsAround() = calculateBombsAround(x,y);
-                }
-            }
-        }
-
-    }
-
-    void initChunks()
-    {
-        mBombsPositions.clear();
-        for(auto &ichunk : mField.cache().chunksTable()) {
-            Chunk *chunk = static_cast<Chunk*>(ichunk.mChunkRec.mChunk);
-            for(auto &bombcell : chunk->mBombsPositions)
-                if(!chunk->isInitialised())
-                    putBombMarkers(ichunk.mChunkRec.mPos * stf::Vec2d{Chunk::Width, Chunk::Height} + bombcell);
-            static_cast<Chunk*>(ichunk.mChunkRec.mChunk)->initialise();
-        }
-    }
+    Cell* put(const stf::Vec2d &pos, Cell* cell);
+    int calculateBombsAround(const int x, const int y);
+    void activateCells(const stf::Vec2d &pos, std::list<stf::Vec2d> &emptyCells);
+    void putBombMarkers(const stf::Vec2d &pos);
+    void initChunks();
 };
 
 
